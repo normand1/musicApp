0328764db5356d5b07c4182f940052bb4e91dbd6
David Norman <david.norman.w@gmail.com>
2014-12-12 21:08:03 -0800

    initial commit

---
+++ musicApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  musicApp
+//
+//  Created by davinorm on 12/12/14.
+//  Copyright (c) 2014 David Norman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
